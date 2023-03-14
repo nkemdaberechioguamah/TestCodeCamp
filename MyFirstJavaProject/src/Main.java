f15f48d64ebb6fb900aa217f5fbdccf3df89e969
nkemdaberechioguamah <nkem.okoronkwo@gmail.com>
2023-03-14 08:26:37 +0100

    Lesson 14 task Introjuction to Java

---
+++ MyFirstJavaProject/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello Testify Academy. My name is Nkem");
+        System.out.println("I am in Team Newman. This is my first Java Project");
+        System.out.println("STST has been interesting so far");
+        System.out.println("It's been challenging too");
+        System.out.println("However I am looking forward to finishing strong");
+        System.out.println("I am also considering TAS");
+        System.out.println("But I am keeping my fingers crossed on that");
+        System.out.println("We will see how it goes");
+        System.out.println("Thank you");
+    }
+}
